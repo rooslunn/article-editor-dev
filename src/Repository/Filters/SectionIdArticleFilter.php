4e87fe93bb3dab229561ed2980865bdcee5c2306
Ruslan Kladko <ruslan.kladko@dreamscapenetworks.com>
2019-10-24 13:50:40 +0300

    fixup! Meet article_list reinvented

---
+++ src/Repository/Filters/SectionIdArticleFilter.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Dreamscape\Repository\Filters;
+
+
+use Dreamscape\Contracts\Database\QueryFilter as QueryFilterContract;
+
+final class SectionIdArticleFilter extends ArticleFilter implements QueryFilterContract
+{
+
+    private $section_id;
+
+    public function __construct($section_id)
+    {
+        $this->section_id = (int) $section_id;
+    }
+
+    public function where()
+    {
+        return $this->where_clause($this->aliased('section_id'), '!=', $this->section_id);
+    }
+
+    public function orderBy()
+    {
+        return null;
+    }
+}
